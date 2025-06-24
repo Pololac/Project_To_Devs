70fcda52f4bc94da5a164290cb84118f487ac404
Pololac <lacostepaul@yahoo.fr>
2025-06-24 16:39:01 +0200

    Ajout classe Skill

---
+++ src/main/java/com/hb/cda/projecttodevs/entity/Skill.java
@@ -0,0 +1,43 @@
+package com.hb.cda.projecttodevs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String name;
+
+    public Skill() {
+    }
+
+    public Skill(String name) {
+        this.name = name;
+    }
+
+    public Skill(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
